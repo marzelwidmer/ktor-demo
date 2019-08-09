0f6c683267d29fb25e7f9b10c1c495ded04d6bd2
Marcel Widmer <marzelwidmer@gmail.com>
2019-08-09 13:57:30 +0200

    Install DefaultHeader and Compression feature.

--- src/module/HtmlDsl.kt
+++ src/module/HtmlDsl.kt
@@ -2,12 +2,18 @@ package ch.keepcalm.ktor.module
 
 import io.ktor.application.Application
 import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.features.Compression
+import io.ktor.features.DefaultHeaders
 import io.ktor.html.respondHtml
 import io.ktor.routing.get
 import io.ktor.routing.routing
 import kotlinx.html.*
 
 fun Application.html() {
+
+    install(DefaultHeaders)
+    install(Compression)
 
     routing {
         get("/html-dsl") {
