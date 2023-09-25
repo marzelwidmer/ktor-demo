3adcdc08ff1dc37c415114c3bc9b95bc1f9c6264
c3smonkey <c3smonkey@gmail.com>
2023-09-25 16:08:23 +0200

    Add Atlas MongoDB configuration

--- src/main/kotlin/com/example/plugins/MongoDB.kt
+++ src/main/kotlin/com/example/plugins/MongoDB.kt
@@ -6,17 +6,14 @@ import org.bson.Document
 
 
 fun configureMongoDB() {
+    val connectionString = System.getenv("MONGODB_URI")?.let { it } ?: "mongodb://root:rootpassword@localhost:27017"
 
-    val username = "root"
-    val password = "rootpassword"
-    val connectionString = System.getenv("MONGODB_URI")?.let { it } ?: "mongodb://$username:$password@localhost:27017"
-
-   MongoClients.create(connectionString).use { mongoClient ->
-       val database = mongoClient.getDatabase("admin")
-       runBlocking {
-           database.runCommand(Document("ping", 1))
-       }
-       println("Pinged your deployment. You successfully connected to MongoDB!")
-   }
+    MongoClients.create(connectionString).use { mongoClient ->
+        val database = mongoClient.getDatabase("admin")
+        runBlocking {
+            database.runCommand(Document("ping", 1))
+        }
+        println("Pinged your deployment. You successfully connected to MongoDB!")
+    }
 }
 
